8a1c833893bf70302f324ef033d82413c5f7b8dd
Max Rukomoynikov <rukomoynikov@gmail.com>
2024-10-02 20:30:14 +0100

    Model level tests

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
+  describe 'associations' do
+    it { should have_many(:comments) }
+  end
+
   describe 'validations' do
     context 'when params are valid' do
       let(:project_attributes) { attributes_for(:project) }
@@ -12,6 +16,10 @@ RSpec.describe Project, type: :model do
           Project.count
         }
       end
+    end
+
+    context 'when params are valid' do
+      it { should validate_length_of(:title).is_at_least(2) }
     end
   end
 end
